981200346447101f809a3f384a52706e8a154aaa
abdullrahmanahmed <abdullrahmanahmed74@gmail.com>
2024-12-10 22:01:30 +0200

    finish create button update

--- main.js
+++ main.js
@@ -8,25 +8,30 @@ let count = document.getElementById("count");
 let category = document.getElementById("category");
 let submit = document.getElementById("submit");
 
-// get total
+let mood = "createdata";
+let tmp;
+
+let datapro = JSON.parse(localStorage.getItem("product")) || [];
 
 function getTotal() {
-  if (price.value != "") {
-    let result = +price.value + +taxes.value + +ads.value - +discount.value;
+  if (
+    price.value !== "" &&
+    taxes.value !== "" &&
+    ads.value !== "" &&
+    discount.value !== ""
+  ) {
+    let result = (
+      +price.value +
+      +taxes.value +
+      +ads.value -
+      +discount.value
+    ).toFixed(2);
     total.innerHTML = result;
     total.style.background = "#040";
   } else {
     total.innerHTML = "";
     total.style.background = "#a00d02";
   }
-}
-
-// create product
-let datapro;
-if (localStorage.product != null) {
-  datapro = JSON.parse(localStorage.product);
-} else {
-  datapro = [];
 }
 
 submit.onclick = function () {
@@ -40,29 +45,27 @@ submit.onclick = function () {
     count: count.value,
     category: category.value,
   };
-  // create count
-  if (newpro.count > 1) {
-    for (let i = 0; i < newpro.count; i++) {
+
+  if (mood === "createdata") {
+    if (newpro.count > 1) {
+      for (let i = 0; i < newpro.count; i++) {
+        datapro.push({ ...newpro });
+      }
+    } else {
       datapro.push(newpro);
     }
   } else {
-    datapro.push(newpro);
+    datapro[tmp] = newpro;
+    mood = "createdata";
+    submit.innerHTML = "Create";
+    count.style.display = "block";
   }
-  // finish count
-
-  // save localstorage
 
   localStorage.setItem("product", JSON.stringify(datapro));
-
-  //  create function clear data
-
   cleardata();
-
-  // read data
   showdata();
 };
 
-// clear inputs
 function cleardata() {
   title.value = "";
   price.value = "";
@@ -74,51 +77,60 @@ function cleardata() {
   category.value = "";
 }
 
-// read
 function showdata() {
+  getTotal();
   let table = "";
-  for (let i = 1; i < datapro.length; i++) {
+  for (let i = 0; i < datapro.length; i++) {
     table += `
-                <tr>
-              <td>${i}</td>
-              <td>${datapro[i].title}</td>
-              <td>${datapro[i].price}</td>
-              <td>${datapro[i].taxes}</td>
-              <td>${datapro[i].ads}</td>
-              <td>${datapro[i].discount}</td>
-              <td>${datapro[i].total}</td>
-              <td>${datapro[i].category}</td>
-              <td><button id="update">update</button></td>
-              <td><button onclick ="deletedata(${i})" id="delete">delete</button></td>
-            </tr>`;
+      <tr>
+        <td>${i + 1}</td>
+        <td>${datapro[i].title}</td>
+        <td>${datapro[i].price}</td>
+        <td>${datapro[i].taxes}</td>
+        <td>${datapro[i].ads}</td>
+        <td>${datapro[i].discount}</td>
+        <td>${datapro[i].total}</td>
+        <td>${datapro[i].category}</td>
+        <td><button onclick="updatedata(${i})">Update</button></td>
+        <td><button onclick="deletedata(${i})">Delete</button></td>
+      </tr>
+    `;
   }
   document.getElementById("tbody").innerHTML = table;
+
   let btndelete = document.getElementById("deleteall");
   if (datapro.length > 0) {
-    btndelete.innerHTML = `
-      <button onclick ="deleteall()">Delete All (${datapro.length})</button>
-    `;
+    btndelete.innerHTML = `<button onclick="deleteall()">Delete All (${datapro.length})</button>`;
   } else {
     btndelete.innerHTML = "";
   }
 }
-showdata();
-// finish read data
 
-// delete
 function deletedata(i) {
   datapro.splice(i, 1);
-  localStorage.product = JSON.stringify(datapro);
+  localStorage.setItem("product", JSON.stringify(datapro));
   showdata();
-  // finish delete  data
 }
 
 function deleteall() {
   localStorage.clear();
-  datapro.splice(0);
+  datapro = [];
   showdata();
 }
 
-// update
-// search
-// clean data
+function updatedata(i) {
+  title.value = datapro[i].title;
+  price.value = datapro[i].price;
+  taxes.value = datapro[i].taxes;
+  ads.value = datapro[i].ads;
+  discount.value = datapro[i].discount;
+  getTotal();
+  count.style.display = "none";
+  category.value = datapro[i].category;
+  submit.innerHTML = "Update";
+  mood = "updatedata";
+  tmp = i;
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+showdata();
